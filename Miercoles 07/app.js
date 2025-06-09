f3422b971c6bf3177d9d01a40cae38c4fe623921
Snapshotss <323030286@upjr.edu.mx>
2025-06-09 13:55:42 -0600

    Actividad-app

---
+++ Miercoles 07/app.js
@@ -0,0 +1,6 @@
+var nombre = 'Toni';
+var apellido = 'Stark';
+apellido = 'Batman';
+nombre = 'Peter Parker';
+console.log(nombre);
+console.log(apellido);
---
+++ Miercoles 07/app.ts
@@ -0,0 +1,10 @@
+
+let nombre = 'Toni';
+let apellido = 'Stark';
+
+apellido = 'Batman';
+
+nombre = 'Peter Parker';
+
+console.log (nombre);
+console.log (apellido);
