c47231580897cf1a831882cce1ba634e6151e730
Snapshotss <323030286@upjr.edu.mx>
2025-06-09 14:00:58 -0600

    Problemas-arrays

---
+++ Problema_Arrays/array.ts
@@ -0,0 +1,15 @@
+
+(() => {
+
+
+const nombre = ['Hugo', 'Paco', 'Luis', 'Pedro', 'Hector'];
+const edad = [35, 50, 40, 18, 30];
+
+// Índice del menor y mayor usando Math y funciones flecha
+const menor = edad.indexOf(Math.min(...edad));
+const mayor = edad.indexOf(Math.max(...edad));
+
+console.log(`${nombre[menor]} es el menor con ${edad[menor]} años.`);
+console.log(`${nombre[mayor]} es el mayor con ${edad[mayor]} años.`);
+
+})();
