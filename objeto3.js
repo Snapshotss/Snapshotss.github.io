42f409449325f1364764fc06517f07ae782fb069
Snapshotss <323030286@upjr.edu.mx>
2025-06-09 15:18:45 -0600

    objetos 3 y 4

---
+++ objeto3.js
@@ -0,0 +1,12 @@
+"use strict";
+//Object with properties
+let planet = {
+    name: "Earth",
+    galaxy: "Milky Way",
+    numberOfMoons: 1,
+    weight: 100000
+};
+console.log("Planet Name :- " + planet.name);
+console.log("Planet Galaxy :- " + planet.galaxy);
+console.log("Planet Number of Moons :- " + planet.numberOfMoons);
+console.log("Planet Weight :- " + planet.weight);
